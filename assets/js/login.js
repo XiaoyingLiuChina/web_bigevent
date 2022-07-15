56aa573a0e6998c87e93793843f7b8cdca8190ca
LiuXiaoying <2214872361@qq.com>
2022-07-15 10:18:14 +0800

    登录注册前端

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,3 +1,11 @@
 $(function(){
-    $('')
-})
+    $('#link-reg').on('click',function(){
+        $('.login-box').hide();
+        $('.register-box').show();
+    });
+
+    $('#link-login').on('click',function(){
+        $('.login-box').show();
+        $('.register-box').hide();
+    })
+});
